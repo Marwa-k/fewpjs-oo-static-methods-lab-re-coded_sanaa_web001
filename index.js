c6f6d7c6fc009dc4762f6bff5420c800166d7d61
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-13 16:56:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,25 +2,29 @@ class Formatter {
   //add static methods here
   
   
-  static capitalize(string) {
+  static capitalize(string)
+  {
     let firstLetter = string.charAt(0).toUpperCase()
     return firstLetter  + string.slice(1)
+   }
+
+  static sanitize(string)
+  {
+      return string.replace(/[^A-Za-z0-9 '-]/g, '') ;
   }
 
-  static sanitize(string) {
-    return string.replace(/[^A-Za-z0-9 '-]/g, '') ;
-  }
-}
 
-   static titleize(string) {
-    let capString = string.charAt(0).toUpperCase() + string.slice(1)
-    let excludedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
-    let stringArray = capString.split(" ")
-    return stringArray.map(string => {
-      if (excludedWords.includes(string)) {
+   static titleize(string) 
+   {
+    let upperString = string.charAt(0).toUpperCase() + string.slice(1)
+    let  expectWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let arr = upperString.split(" ")
+    return arr.map(string => {
+      if (expectWords.includes(string)) {
         return string
       } else {
         return string.charAt(0).toUpperCase() + string.slice(1)
       }
     }).join(" ")
   }
+}
